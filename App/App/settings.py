fcfd73562cef389388f851ad9cd76f50789129a6
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-11-11 20:29:33 -0300

    Update: Adding <NEW> button and styling

--- App/App/settings.py
+++ App/App/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_FILES_DIRS = [
+    Path(BASE_DIR, 'static/'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
