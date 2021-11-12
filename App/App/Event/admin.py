3182449e68a4b2a68d55e923a2a5fef64efd9aa7
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-11-12 00:06:50 -0300

    Update: Adding two new models, Profile and Attendance model.

--- App/App/Event/admin.py
+++ App/App/Event/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Events
+from .models import Events, Profile, Attendance
 
 admin.site.register(Events)
+admin.site.register(Profile)
+admin.site.register(Attendance)
--- App/App/Event/models.py
+++ App/App/Event/models.py
@@ -10,3 +10,18 @@ class Events(models.Model):
 
     __str__ = lambda self: self.event_name
 
+class Profile(models.Model):
+    profile_id = models.AutoField(primary_key=True)
+    profile_name = models.CharField(max_length=100)
+    profile_email = models.EmailField()
+
+    __str__ = lambda self: self.profile_name
+
+class Attendance(models.Model):
+    attendance_id = models.AutoField(primary_key=True)
+    attendance_event_id = models.ForeignKey(Events, null=False, blank=False, on_delete=models.CASCADE) # WARNING
+    attendance_profile_id = models.ForeignKey(Profile, null=False, blank=False, on_delete=models.CASCADE) # WARNING
+    attendance_date = models.DateTimeField(auto_now_add=True)
+    attendance_status = models.BooleanField(default=True)
+
+    __str__ = lambda self: self.attendance_profile_id
