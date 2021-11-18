7ee60b8e247785ac9d61600729f29e8409bfb5c6
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2021-11-18 16:27:56 -0300

    Update: CSS has been imported in HTML file, but doesn't update it0

--- App/settings.py
+++ App/settings.py
@@ -121,10 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_FILES_DIRS = [
-    Path(BASE_DIR, 'static/'),
+# add static files
+STATICFILES_DIRS = [
+    # str(BASE_DIR.joinpath('static'))
+    Path(BASE_DIR, 'static'),
 ]
+
+# STATIC_FILES_DIRS = [
+#     Path(BASE_DIR, 'static/'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
